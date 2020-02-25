b30a6a339a2da7424c4acd555b283a33e0b9229a
Colin Hutchinson <chutchic@gmail.com>
2020-02-25 15:28:11 +0000

    chore(ci) remove uneeded bash script

--- .ci/run_tests.sh
+++ .ci/run_tests.sh
@@ -21,7 +21,21 @@ else
 fi
 
 if [ "$TEST_SUITE" == "integration" ]; then
-    eval "$TEST_CMD" spec/02-integration/
+    if [[ "$TEST_SPLIT" == first* ]]; then
+        # GitHub Actions, run first batch of integration tests
+        eval "$TEST_CMD" $(ls -d spec/02-integration/* | head -n4)
+
+    elif [[ "$TEST_SPLIT" == second* ]]; then
+        # GitHub Actions, run second batch of integration tests
+        # Note that the split here is chosen carefully to result
+        # in a similar run time between the two batches, and should
+        # be adjusted if imbalance become significant in the future
+        eval "$TEST_CMD" $(ls -d spec/02-integration/* | tail -n+5)
+
+    else
+        # Non GitHub Actions
+        eval "$TEST_CMD" spec/02-integration/
+    fi
 fi
 
 if [ "$TEST_SUITE" == "dbless" ]; then
